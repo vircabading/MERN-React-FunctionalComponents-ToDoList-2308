c54643c9ac71b164a68aba479921f759a71e90f3
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-08-05 00:10:22 -0500

    add navbar

---
+++ src/components/NavBarView.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../App.css';
+
+///////////////////////////////////////////////////
+//  NAVIGATION BAR COMPONENT
+///////////////////////////////////////////////////
+
+const NavBarView = () => {
+    return (
+        <header>
+            <div className='navbar navbar-dark bg-navbar-dark box-shadow'>
+                <div className='container d-flex justify-content-between'>
+                    {/* **** Site Title ******** */}
+                    <h2 className='text-light-color'><strong>📃 To Do List</strong></h2>
+                    {/* **** Link to The Root of the Site ******** */}
+                        <a 
+                            className='btn btn-sm round-btn bg-nav-button'
+                            href='/' >
+                            <strong className='text-white'>🏡 Home</strong>
+                        </a>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default NavBarView;
